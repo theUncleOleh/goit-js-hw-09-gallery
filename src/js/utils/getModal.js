31145cf25133c2f4edebab31882152d08fbfd22b
Oleh Semennykov <sumcue@gmail.com>
2021-07-25 17:43:31 +0300

    finish work

---
+++ src/js/utils/getModal.js
@@ -0,0 +1,50 @@
+// export const getModal = () => {
+   
+// const lightBox = document.querySelector(".js-lightbox");
+//   const lightBoxOverlay = document.querySelector(".lightbox__overlay");
+//   const lightBoxContent = document.querySelector(".lightbox__content");
+//   const lightBoxImg = document.querySelector(".lightbox__image");
+//   const lightBoxBtn = document.querySelector(".lightbox__button");
+
+ 
+// // вешаем обозревателя на клик
+//   galleryContainer.addEventListener('click', onOpenModal)
+    
+
+//   //  открытие модального окна
+//    function onOpenModal (event){
+//      event.preventDefault();
+//      window.addEventListener('keydown',onEscKeyDown );
+//      const galeryImage = event.target;
+//      if(galeryImage.nodeName !== 'IMG'){
+//        return;
+//      };
+//      lightBox.classList.add('is-open');
+
+   
+//      lightBoxImg.src = galeryImage.dataset.source;
+//      lightBoxImg.alt = galeryImage.alt;
+//      console.log(lightBoxImg.src);
+//     //  const carentActiveImg = document.querySelector("")
+
+//    };
+
+//    lightBoxBtn.addEventListener('click',onCloseModal,);
+
+//   function onCloseModal(event){
+//     window.removeEventListener('keydown',onEscKeyDown );
+//     lightBox.classList.remove('is-open');
+//     //  lightBoxImg.src = "";
+//      console.log(lightBoxImg);
+//   };
+
+//    function onEscKeyDown (evt){
+//      console.log(evt);
+     
+//      if(evt.code === 'Escape') {
+//       // lightBox.classList.remove('is-open');
+//       onCloseModal();
+//      }
+//    }
+  
+// };
