eefe9fdc09ccd7c6a6a0b437de15f54c9d6406e7
Oleh Semennykov <sumcue@gmail.com>
2021-08-08 22:36:46 +0300

    gallery

--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
 // import './sass/main.scss';
 import debounce from 'lodash.debounce';
 import trottle from 'lodash.throttle';
-// import 'baguettebox.js';
+// import baguetteBox  from  'baguettebox.js' ;
 import {galleryContainer} from './js/utils/galleryContainer';
 // import { baguetteBox } from './js/utils/arrow';
-
+import {galleryItems} from './js/utils/galleryItems'
+import { galeryItemList} from './js/utils/galleryContainer';
 // console.log(baguettebox);
-
+// console.log(galeryItemList);
 const lightBox = document.querySelector(".js-lightbox");
   const lightBoxOverlay = document.querySelector(".lightbox__overlay");
   const lightBoxContent = document.querySelector(".lightbox__content");
   const lightBoxImg = document.querySelector(".lightbox__image");
   const lightBoxBtn = document.querySelector(".lightbox__button");
 
-  galleryContainer.addEventListener('click', e => {
-   
-    let currentIndex = document.querySelector("gallery__image[data-index]");
-    console.log(currentIndex.dataset.index);
-  })
+  // galleryContainer.addEventListener('click', e => {
+    console.log(galleryContainer);
+  //   let currentIndex = document.querySelector("gallery__image[data-index]");
+  //   console.log(currentIndex);
+  // })
 // вешаем обозревателя на клик
+
+galleryContainer.addEventListener('click', onClick);
+function onClick (evt) {
+
+};
+
+// console.log(currentIndex);
   galleryContainer.addEventListener('click', onOpenModal)
   // const currentIndex =  createItem.dataset.index;
   //  открытие модального окна
 //   const gallery = baguetteBox.run('.gallery');
 // baguetteBox.show(index, gallery[0]);
 
+// console.log(currentIndex);
    function onOpenModal (event){
      event.preventDefault();
-     window.addEventListener('keydown',onEscKeyDown );
-     const galeryImage = event.target;
+    window.addEventListener('keydown',onEscKeyDown );
+    galleryContainer.addEventListener('keydown', onArrowRight, onArrowLeft);
+ 
+     let galeryImage = event.target;
+    let currentIndex = event.target.dataset.index;
+ 
 
-// console.log(currentIndex);
+
 
   if(galeryImage.nodeName !== 'IMG'){
        return;
@@ -40,35 +53,46 @@ const lightBox = document.querySelector(".js-lightbox");
    
      lightBoxImg.src = galeryImage.dataset.source;
      lightBoxImg.alt = galeryImage.alt;
-    //  console.log(lightBoxImg.src);
+      
+     function onArrowRight() {
+    if(currentIndex + 1 > galleryItems.lenght -1) {
+        currentIndex = 0;
+
+    } else{
+        currentIndex += 1;
+    }
+    lightBoxImg.src = galeryImage.dataset.source,
+    lightBoxImg.alt = galeryImage.alt;
+       
+    
+  
+
+  
+}; 
+   function onArrowLeft (evt){
+if(currentIndex - 1 < 0 ) {
+  currentIndex = galleryItems.length - 1;
+  } else {
+    currentIndex -= 1;
+  }
+  lightBoxImg.src = galeryImage.dataset.source;
+  lightBoxImg.alt = galeryImage.alt;
+}
 
   };
+  
     //  const carentActiveImg = document.querySelector("")
 
 
     
-//     galleryContainer.addEventListener('keydown', onArrowRight)
+   
    
     
      
      
-//      function onArrowRight() {
-    
-
-//          if(currentIndex + 1 > galleryItems.lenght -1) {
-//              currentIndex = 0;
-// //  console.log(currentIndex);
-//          } else{
-//              currentIndex += 1;
-//          }
-         
-//          ligthBoxImg(
-//           galleryItems [currentIndex].original,
-//           galleryItems  [currentIndex].description
-//              );
-//    }; 
+  
    
-   lightBoxBtn.addEventListener('click',onCloseModal,);
+   lightBoxBtn.addEventListener('click', onCloseModal);
 
   function onCloseModal(event){
     window.removeEventListener('keydown',onEscKeyDown );
@@ -79,19 +103,47 @@ const lightBox = document.querySelector(".js-lightbox");
 
    function onEscKeyDown (evt){
      console.log(evt);
-     
+    
      if(evt.code === 'Escape') {
       // lightBox.classList.remove('is-open');
       onCloseModal();
      }
    };
+   
+//    function onArrowRight() {
+//     if(currentIndex + 1 > galleryItems.lenght -1) {
+//         currentIndex = 0;
 
+//     } else{
+//         currentIndex += 1;
+//     }
+//     lightBoxImg(
+//       galleryItems[currentIndex].original,
+//        galleryItems[currentIndex].description
+       
+//     );
+  
+
+  
+// }; 
+//    function onArrowLeft (evt){
+// if(currentIndex - 1 < 0 ) {
+//   currentIndex = galleryItems.length - 1;
+//   } else {
+//     currentIndex -= 1;
+//   }
+//   lightBoxImg(
+//     galleryItems[currentIndex].original,
+//     galleryItems[currentIndex].description
+//   );
+// }
   //  попробуем реализовать слайдер
   // 1 повесим слушатель на модальное окно
   //  определим функцию прокрутки вправо и влево
   // export const arrowImg = () => {
   
    
+
      
 
   
